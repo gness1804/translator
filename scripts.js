ea7305a729c79c95f875a83228d8911e53961acc
gness1804 <graham.nessler@gmail.com>
2017-02-17 10:14:44 -0700

    Got basic revised logic working

--- scripts.js
+++ scripts.js
@@ -13,26 +13,44 @@ const isTheLetterAVowel = (letter) => {
 }
 
 //this is the main function
-const pigLatinTrans = (word) => {
-  if (typeof word !== 'string') {
+const pigLatinTrans = (text) => {
+  const masterArr = []
+
+  if (typeof text !== 'string') {
     throw new Error('You must enter a string.')
   }
-  const wordArr = word.toLowerCase().split('')
-  if (wordArr.includes(' ')) {
-    throw new Error('You must enter a single word.')
+
+  const translate = (word) => {
+    const wordArr = word.split('')
+    if (isTheLetterAVowel(wordArr[0])) {
+      masterArr.push(`${wordArr.join('')}way`)
+    } else {
+      const secondArr = []
+      secondArr.push(wordArr[0])
+      masterArr.push(`${wordArr.slice(1).concat(secondArr).join('')}ay`)
+    }
   }
-  if (isTheLetterAVowel(wordArr[0])) {
-    return `${wordArr.join('')}way`
-  } else {
-    const secondArr = []
-    secondArr.push(wordArr[0])
-    return `${wordArr.slice(1).concat(secondArr).join('')}ay`
+
+  const textArr = text.toLowerCase().split(' ')
+
+  for (var i = 0; i < textArr.length; i++) {
+    translate(textArr[i])
   }
+
+  return masterArr
+
+  // if (isTheLetterAVowel(wordArr[0])) {
+  //   return `${wordArr.join('')}way`
+  // } else {
+  //   const secondArr = []
+  //   secondArr.push(wordArr[0])
+  //   return `${wordArr.slice(1).concat(secondArr).join('')}ay`
+  // }
 }
 
 console.log('consonant test:', pigLatinTrans(dog)) //returns 'ogday'
 console.log('vowel test:', pigLatinTrans(ember)) //returns 'emberway'
 console.log('consonant capital test:', pigLatinTrans('Dog'))
 console.log('vowel capital test:', pigLatinTrans('Egg'))
+console.log('multi-word test:', pigLatinTrans('hello there'))
 console.log('error test for non-string:', pigLatinTrans(9)) //throws an error
-console.log('error test for multi-word string:', pigLatinTrans('Hello there')) //throws an error
